77be4c30247e1ccd55e8e7b1652cb4cb7002a62c
19tylermalone94 <19tylermalone94@gmail.com>
2023-12-06 17:14:53 -0700

    sum of the totients

--- problem 72/CountingFracs.java
+++ problem 72/CountingFracs.java
@@ -1,41 +1,23 @@
-import java.util.BitSet;
 
 public class CountingFracs {
 
-    static BitSet primes = new BitSet();
-
-    static void genPrimes() {
-        for (int i = 2; i <= 1000000; ++i) {
-            if (isPrime(i, 2)) {
-                primes.set(i);
-            } else {
-                primes.clear(i);
-            }
-        }
-    }
-
-    static boolean isPrime(int n, int i) {
-        return (i * i > n) ? n > 1 : (n % i == 0) ? false : isPrime(n, i + 1);
-    }
-
-    // n * PI p|n (1 - 1/p)
-    static double totient(int n) {
-        double product = 1;
-        for (int i = 2; i * i <= n; i += ( i == 2) ? 1 : 2) {
+    static int totient(int n) {
+        int result = n;
+        for (int i = 2; i <= n / i; i++) {
             if (n % i == 0) {
-                if (primes.get(i)) {
-                    product *= 1.0 - (1.0 / i);
+                while (n % i == 0) {
+                    n /= i;
                 }
-                if (n / i != i && primes.get(n / i)) {
-                    product *= 1.0 - (1.0 / (n / i));
-                }
+                result -= result / i;
             }
         }
-        return (product == 1) ? n - 1 : n * product;
+        if (n > 1) {
+            result -= result / n;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        genPrimes();
         long sum = 0;
         for (int i = 2; i <= 1000000; ++i) {
             sum += totient(i);
