9d9a465c8b771a479b78b5189a905e7af1ffa588
19tylermalone94 <19tylermalone94@gmail.com>
2023-12-09 13:08:58 -0700

    counting sums

--- problem 76/CountingSums.java
+++ problem 76/CountingSums.java
@@ -1,7 +1,17 @@
 public class CountingSums {
 
     public static void main(String[] args) {
-        
+        System.out.println(makeChange(100, 99));
+    }
+
+    static long makeChange(long amount, long coin) {
+        if (amount == 0 || coin == 1) {
+            return 1;
+        } else if (coin > amount) {
+            return makeChange(amount, coin - 1);
+        } else {
+            return makeChange(amount - coin, coin) + makeChange(amount, coin - 1);
+        }
     }
 
 }
